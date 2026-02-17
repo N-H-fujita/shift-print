e3caece8a55acee60ad891a37da9fd1c3b814c4d
N-H-fujita <86991819+N-H-fujita@users.noreply.github.com>
2026-02-17 20:12:34 +0900

    chore: プロジェクト初期構成を追加

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return <h1>Shift Print Tool</h1>
+}
+
+createRoot(document.getElementById('root')!).render(<App />);
